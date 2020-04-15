5969b42e706cd5bdae8af85f58d2ee80aa55860c
Sandie Nunez <snunez@vt.edu>
2020-04-15 21:13:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,37 +7,19 @@ class School
   end 
   
    def add_student(name_student, level)
-    roster[level] = []
+   #  binding.pry
+   # if !roster.has_key?(level)
+      roster[level] ||= [] 
+   # end 
     roster[level] << name_student
   end 
   
-# def grade(grade)
-#     @roster[grade]
-#   end
-# end 
+def grade(grade)
+    @roster[grade]
+  end
 
-#   def sort
-#     @roster.each do |key, value|
-#       value.sort!
-#     end
-# end 
-
-  # def add_student(name, grade)
-  #   if @roster[grade] != nil
-  #     @roster[grade] << name
-  #   else
-  #     @roster[grade] = [name]
-  #   end
-  # end
-
-  # def grade(grade)
-  #   @roster[grade]
-  # end
-
-  # def sort
-  #   @roster.each do |key, value|
-  #     value.sort!
-  #   end
-  # end
-  
- ÷
+def sort
+ # binding.pry 
+roster.each {|level, students| roster[level]=students.sort}
+end 
+end 
